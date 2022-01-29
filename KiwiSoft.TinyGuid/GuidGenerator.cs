ba4546b437ca4689ea17a06847e1c79f0776fbd7
John Hadikusumo <john.hadikusumo@gmail.com>
2022-01-29 18:14:47 +1100

    update

--- KiwiSoft.TinyGuid/GuidGenerator.cs
+++ KiwiSoft.TinyGuid/GuidGenerator.cs
@@ -1,9 +1,10 @@
-﻿using System;
-
-namespace KiwiSoft.TinyGuid
+﻿namespace KiwiSoft.TinyGuid
 {
     public class GuidGenerator
     {
+        public GuidGenerator()
+        {
 
+        }
     }
 }
